c1d469280c910d7d9ba2dc9f84043e13144d71e0
Adam Blankenship <adam@abctahoe.net>
2014-08-26 00:13:06 -0700

    Updated subcellular

---
+++ subcellular/js/min/script.js
@@ -0,0 +1,1 @@
+!function(a){Drupal.behaviors.theme={attach:function(){a(".jAccordion").jAccordion(),a(".jBlocklink").jBlocklink(),a("form").jFormal(),a(".jMmenu").jMmenu(),a(".jScrolli").jScrolli(),a(".jTabs").jTabs()}}}(jQuery);
