ab3474b0b9f0e1436e09fc22a9e239d41c132c44
Adam Blankenship <adam@abctahoe.net>
2014-09-15 15:56:52 -0700

    Deleted compiled resources to increase the likelihood of a meaningful code review

---
+++ subcellular/js/script.js
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * Custom javascript for use in your theme.
+ */
+
+(function($) {
+
+  Drupal.behaviors.theme = {
+    attach: function(context, settings) {
+      // Call cellular UI functions:
+      $('.jAccordion').jAccordion({
+      });
+      $('.jBlocklink').jBlocklink({
+      });
+      $('.jTabs').jTabs({
+        // "orient": "vertical"
+      });
+      $('form').jFormal();
+      $('#nav').jMmenu();
+      $('.jScrolli').jScrolli();
+
+      /* // Functions under development:
+       // $('#nav').jStickyheader();
+       // $('.jParallax').jParallax();
+       // $('.jScrolltrigger').jScrolltrigger();
+       // $('.jCarousel').jCarousel();
+       // $('.jEqualheight').jEqualheight();
+       // $('.jToggle').jToggle();
+       // $('.jSticky').jSticky();
+       */
+      /*
+       // Call Masonry:
+       $('.block-content').masonry({
+       containerStyle: null,
+       itemSelector: '.blocklink-wrap',
+       isWitWidth: true,
+       columnWidth: 240
+       });
+       */
+
+      /* End Drupal.behaviors.theme */
+    }
+  };
+})(jQuery);
