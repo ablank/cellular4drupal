a6a1d18254f51f4446ab5a8ac6b6b77d367b1e7c
Adam Blankenship <adam@abctahoe.net>
2014-09-29 22:53:54 -0700

    Cleaning up

--- cellular/js/script.js
+++ cellular/js/script.js
@@ -3,22 +3,49 @@
  * Custom javascript for use in your theme.
  */
 
-(function($) {
+(function ($) {
 
   Drupal.behaviors.theme = {
-    attach: function(context, settings) {
-      
-       // Call cellular UI:
-       $('.jAccordion').jAccordion({
-       });
-       $('.jBlocklink').jBlocklink({
-       });
-       $('.jTabs').jTabs({
-       // "orient": "vertical"
-       });
-       $('form').jFormal();
-       $('#nav').jMmenu();
-       $('.jScrolli').jScrolli();
+    attach: function (context, settings) {
+jQuery(window).bind('scroll', function(e){
+  e.preventDefault();
+  console.log(e);
+});
+      // Cellular UI:
+      $('.jAccordion').jAccordion({
+        "active": 0, // Index value of initial content to display.
+        "duration": 500, // Duration of transition.
+        "easing": "swing", // Type of easing.
+        "single": false // Allow multiple panels to be opened or only 1?
+      });
+      $('.jBlocklink').jBlocklink({
+        "cclass": "jBlocklink-link"
+      });
+      $('form').jFormal({
+        "inputs": [ // Array of elements to format.
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="password"]',
+          'textarea'
+        ]
+      });
+      $('#nav').jMmenu({
+        // Window breakpoint trigger:
+        // "breakpoint": cellular.opts.breakpoint,
+        // Classes added for styling.
+        "cclass": "jMmenu",
+        "direction": "right",
+        "type": "slide"
+      });
+      $('.jScrolli').jScrolli({
+        "active": 0, // Array index of initially active content.
+        "speed": 500, // Duration of cycle.
+        "pause": 3000 // Time to pause between cycles.
+      });
+      $('.jTabs').jTabs({
+        "active": 0, // Array index of initially active content.
+        "orient": "horizontal" // || 'vertical'
+      });
       /* */
       /*
        // Call Masonry:
