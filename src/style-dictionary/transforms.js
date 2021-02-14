02b798a2ffc99be1f6806cfd9970ee2616cf122d
Adam Blankenship <i.adambear@gmail.com>
2021-02-14 14:05:11 -0800

    working on style-dictionary

--- src/style-dictionary/transforms.js
+++ src/style-dictionary/transforms.js
@@ -1,40 +1,56 @@
-"adjust" = {
-  "color": function ($color = "{color.primary.value}", $percent = "{color.adjust.value}") {
-    return color;
-  },
-  "size": function ($base = "{size.default.value}", $percent = "{size.adjust.value}") {
-    return $base * $percent;
-  },
-  "opacity": function ($base = "{size.default.value}", $percent = "{size.adjust.value}") {
-    return $base * $percent;
+const StyleDictionary = require('style-dictionary').extend('./src/style-dictionary/config.json');
+/*
+function LightenDarkenColor(col, amt) {
+  var usePound = false;
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
   }
-}
+  var num = parseInt(col, 16);
+  var r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+  var b = ((num >> 8) & 0x00FF) + amt;
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+  var g = (num & 0x0000FF) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
 
-
-function throwSizeError(name, value, unitType) {
-  throw `Invalid Number: '${name}: ${value}' is not a valid number, cannot transform to '${unitType}' \n`;
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
 }
-/**
- * @namespace Transforms
- */
-module.exports = {
-  /**
-   * Lightens or darkens color based on options value
-   *
-   * @memberof Transforms
-   */
-  'color/darkenlighten': {
-    type: 'value',
-    matcher: isColor,
-    transformer: function (prop) {
-      var color = Color(prop.value);
-      var o = color.toHsl()
-      var vals = `${Math.round(o.h)} ${Math.round(o.s * 100)}% ${Math.round(o.l * 100)}%`
-      if (color.getAlpha() === 1) {
-        return `hsl(${vals})`
-      } else {
-        return `hsl(${vals} / ${o.a})`
-      }
+*/
+/*
+StyleDictionary.registerTransform({
+  name: 'size/scale',
+  type: 'value',
+  matcher: function (prop) {
+    return prop.attributes.category === 'breakpoint';
+  },
+  transformer: function (prop) {
+    var scale = prop.attributes.scale;
+
+    return scale;
+  }
+});
+*/
+/*
+StyleDictionary.registerTransform({
+  name: 'color/lighten',
+  type: 'value',
+  matcher: function(prop) {
+    return prop.attributes.category === 'color';
+  },
+  transformer: function (prop) {
+    var color = Color(prop.value);
+    var o = color.toHsl()
+    var vals = `${Math.round(o.h)} ${Math.round(o.s * 100)}% ${Math.round(o.l * 100)}%`
+    if (color.getAlpha() === 1) {
+      return `hsl(${vals})`
+    } else {
+      return `hsl(${vals} / ${o.a})`
     }
   }
-};
+});
+*/
+module.exports = StyleDictionary;
