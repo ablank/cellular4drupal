24ef20ec4f167c9ddf57a601c8180f2c4d751aed
Adam Blankenship <i.adambear@gmail.com>
2021-04-11 11:15:59 -0700

    updating build

--- src/build/icons.js
+++ src/build/icons.js
@@ -2,25 +2,27 @@ const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const { replace } = require('lodash');
 
 const src = [path.resolve('src', 'assets', 'icons', 'svg')];
-const destination = path.resolve('dist', 'assets', 'icons', 'png');
 
-async function process(dir, svg) {
-  // console.log(`\nFull Path: ${dir}${svg}`);
-  // console.log(`\nName: ${name}`);
-  const input = `${dir}${svg}`;
-  await fs.readFile(input, processSharp(input));
-}
-process().catch((error) => {console.error(error)});
+const process = async (imagepath, name) => {
+  const destination = path.resolve('dist', 'assets', 'icons', 'png', name.replace(/.svg/, '.png'));
+ // const destination = name.replace('.svg', '.png');
 
-function processSharp(input) {
-  console.log(destination, input.replace(/.svg/g, '.png'));
-  sharp(input)
-  //.png()
-  .toFile(path.resolve(destination, input.replace(/.svg/g, '.png')))
-  .catch((error) => { console.error(error) } );
-}
+ console.log(`Imagepath: ${imagepath}`);
+ console.log(`Name: ${name}`);
+ console.log(`Destination: ${destination}`);
+ 
+  await sharp(path.resolve(imagepath, name))
+    .toFile(destination)
+    .then((info) => {
+      console.log(info);
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
 
 src.forEach((directory) => {
   fs.readdir(directory, (e, files) => {
@@ -29,27 +31,3 @@ src.forEach((directory) => {
     });
   });
 });
-
-/**
- * movies.forEach(function(movie) {
-  var downloadPath = './img/posters/' + movie.id + '-300x446.jpg';
-
-  fs.stat(downloadPath, function(err, stat) {
-    if(err == null) {
-      console.log('file exists');
-      return;
-    }
-
-    download(movie.poster, downloadPath, function(){
-      var resizePath = './img/posters/' + movie.id + '-150x223.jpg'; 
-
-      sharp(downloadPath)
-       .resize(150, 223)
-       .toFile(resizePath, function(err) {
-        // output.jpg is a 300 pixels wide and 200 pixels high image
-        // containing a scaled and cropped version of input.jpg
-       });
-    });
-  });
-});
- */
