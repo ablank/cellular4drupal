7c4ca0330d0e3f46d54f0ab785e86ad496964d80
Adam Blankenship <i.adambear@gmail.com>
2021-04-18 17:44:56 -0700

    fix icon build process

--- src/build/icons.js
+++ src/build/icons.js
@@ -1,37 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
-const { replace } = require('lodash');
-let countIcons = 0;
 
-const src = [path.resolve('src', 'assets', 'icons', 'svg')];
-
-const process = async (imagepath, name) => {
-  const destination = path.resolve('dist', 'assets', 'icons', 'png', name.replace(/.svg/, '.png'));
- // const destination = name.replace('.svg', '.png');
-
-//  console.log(`Imagepath: ${imagepath}`);
-//  console.log(`Name: ${name}`);
-//  console.log(`Destination: ${destination}`);
- 
-  await sharp(path.resolve(imagepath, name))
-    .png()
-    .toFile(destination)
-    .then((info) => {
-      console.log(info);
-    })
-    .catch((e) => {
-      console.error(e);
+const createDir = (pathname) => {
+  try {
+    fs.mkdir(pathname, {
+      recursive: true
+    }, (e) => {
+      if (e) {
+        console.error(e)
+      } else {
+        process.chdir(pathname)
+        console.log(`\ndestination: ${pathname}`)
+      }
     });
+  } catch (e) {
+    console.error(e)
+  }
 };
 
-src.forEach((directory) => {
+const processIcon = async (imagepath, name) => {
+  const destination = path.resolve(__dirname, '..', '..', 'dist', 'assets', 'icons', 'png')
+
+  if (!fs.existsSync(destination)) {
+    await createDir(destination);
+  }
+  
+    await sharp(path.resolve(imagepath, name))
+      .png()
+      .toFile(path.resolve(destination, name.replace(/.svg/, '.png')))
+      .then((info) => {
+        console.log(info)
+      })
+      .catch((e) => {
+        console.error(e)
+      });
+      
+};
+
+[path.resolve(__dirname, '..', 'assets', 'icons', 'svg')].forEach((directory) => {
+  //console.log(directory);
   fs.readdir(directory, (e, files) => {
     files.forEach((svg) => {
-      process(directory, svg);
-      countIcons += 1;
+      processIcon(directory, svg)
     });
   });
 });
-
-console.log('\nIcon Count: '+ countIcons);
