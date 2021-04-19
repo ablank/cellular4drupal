d51f94c61f0742dad989335ae8088f797514e3d4
Adam Blankenship <i.adambear@gmail.com>
2021-04-18 18:17:57 -0700

    fixed icon build

--- src/build/icons.js
+++ src/build/icons.js
@@ -4,14 +4,11 @@ const sharp = require('sharp');
 
 const createDir = (pathname) => {
   try {
-    fs.mkdir(pathname, {
-      recursive: true
-    }, (e) => {
+    fs.mkdir(pathname, (e) => {
       if (e) {
         console.error(e)
       } else {
-        process.chdir(pathname)
-        console.log(`\ndestination: ${pathname}`)
+        console.log(`\n Creating directory: ${pathname}`)
       }
     });
   } catch (e) {
@@ -25,21 +22,21 @@ const processIcon = async (imagepath, name) => {
   if (!fs.existsSync(destination)) {
     await createDir(destination);
   }
-  
-    await sharp(path.resolve(imagepath, name))
-      .png()
-      .toFile(path.resolve(destination, name.replace(/.svg/, '.png')))
-      .then((info) => {
-        console.log(info)
-      })
-      .catch((e) => {
-        console.error(e)
-      });
-      
+
+  process.chdir(destination);
+
+  await sharp(path.resolve(imagepath, name))
+    .png()
+    .toFile(path.resolve(destination, name.replace(/.svg/, '.png')))
+    .then((data) => {
+      console.log(`${name.replace(/.svg/, '.png')} created`)
+    })
+    .catch((e) => {
+      console.error(e)
+    });
 };
 
 [path.resolve(__dirname, '..', 'assets', 'icons', 'svg')].forEach((directory) => {
-  //console.log(directory);
   fs.readdir(directory, (e, files) => {
     files.forEach((svg) => {
       processIcon(directory, svg)
